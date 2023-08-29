3fa93180fb8bf7491a745bfdc2022036118a384a
Farzanafaizi <farzana.faizi34@gmail.com>
2023-08-29 13:41:12 +0500

    completed the templates

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-  title =  models.CharField(max_length=200, unique=True)
-  slug =   models.SlugField(max_length=200, unique=True)
+  title =  models.CharField(max_length=300, unique=True)
+  slug =   models.SlugField(max_length=300, unique=True)
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
   created_on = models.DateTimeField(auto_now_add=True)
   updated_on = models.DateTimeField(auto_now=True)
